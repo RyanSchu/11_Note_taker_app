379e499d09ac14d9ca6d92059e398bb4dbab0207
Ryan Schubert <31641912+RyanSchu@users.noreply.github.com>
2022-08-24 21:54:41 -0500

    figured out why request body was undefined

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -33,14 +33,16 @@ const getNotes = () =>
     },
   });
 
-const saveNote = (note) =>
-  fetch('/api/notes', {
+const saveNote = (note) =>{
+  // console.log(note)
+  return fetch('/api/notes', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(note),
   });
+}
 
 const deleteNote = (id) =>
   fetch(`/api/notes/${id}`, {
@@ -71,6 +73,7 @@ const handleNoteSave = () => {
     title: noteTitle.value,
     text: noteText.value,
   };
+  // console.log(newNote)
   saveNote(newNote).then(() => {
     getAndRenderNotes();
     renderActiveNote();
--- server.js
+++ server.js
@@ -6,30 +6,36 @@ const PORT = 3001
 
 const app = express()
 app.use(express.static(__dirname + '/public'));
-
+app.use(express.json())
 
 // get route for homepage
 app.get('/',(req,res) => {
     // console.log(path.join(__dirname, '/public/index.html'))
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
-
+// get route for notes page
 app.get('/notes',(req,res) => {
     // console.log(path.join(__dirname, '/public/index.html'))
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 })
 
 // we need a get a post and a delete method for the notes
+// get is straight forward
 app.get('/api/notes',(req,res) => {
     res.json(notes)
 })
 
+// post request
 app.post('/api/notes',(req,res) => {
-    res.json(repos)
+    // console.log(notes)
+    console.log(req.body)
+    // console.log(res.body)
+    const { noteTitle, noteText } = req.body
+
 })
   
 app.delete('/api/notes',(req,res) => {
-    res.json(repos) 
+    res.json(notes) 
 })
   
 // TODO: Have the app listen on port 3001
